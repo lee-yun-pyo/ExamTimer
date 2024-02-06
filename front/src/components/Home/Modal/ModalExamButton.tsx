f31f94861862a3744bf8c99e9957036dde08a2e8
이윤표 <@naver.com>
2024-02-06 21:10:42 +0900

    #8 feat:  모달 시험 버튼 디자인 변경

--- front/src/components/Home/Modal/ModalExamButton.tsx
+++ front/src/components/Home/Modal/ModalExamButton.tsx
@@ -6,18 +6,18 @@ interface Props {
 export function ModalExamButton({ title, time, onClick }: Props) {
   return (
     <div
-      className="w-full py-3 px-6 flex items-center bg-examBtn-default dark:bg-examBtn-dark rounded-xl"
+      className="w-full py-3 px-6 flex flex-col gap-px bg-examBtn-default dark:bg-examBtn-dark rounded-xl"
       onClick={onClick}
     >
-      <span className="text-xl font-semibold text-text-default dark:text-text-dark">
+      <span className="text-base font-semibold text-text-default dark:text-text-dark">
         {title}
       </span>
-      <div className="flex flex-col ml-6 gap-1">
-        <span className="text-base font-semibold text-text-default dark:text-text-dark">
+      <div className="flex gap-2">
+        <span className="text-sm text-text-default dark:text-text-dark">
           {time}
         </span>
-        <span className="text-base text-text-default dark:text-text-dark">
-          80분
+        <span className="text-sm text-text-default dark:text-text-dark">
+          (80분)
         </span>
       </div>
     </div>
